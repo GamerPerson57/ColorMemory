468af7db9cf0fd63a20d97ce8dc1a64833fce377
GamerPerson57 <amlorden21@gmail.com>
2025-11-13 11:10:00 -0500

    two cards

--- code.js
+++ code.js
@@ -1,20 +1,26 @@
-import { Card } from "./card.js";
+// imports 
+import { MemoryButton } from "./memoryButton.js";
+import { Toolbox } from "./toolbox.js";
 
+// canvas, pencil, and toolbox
 let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
+let toolbox = new Toolbox();
 
-let card = new Card();
+// assign one color to two cards
+let color1 = toolbox.getRandomColor();
+let card1a = new MemoryButton(canvas, pencil, 50, 50, color1);
+let card1b = new MemoryButton(canvas, pencil, 200, 50, color1);
 
+
+// game loop
 function gameLoop() {
-
+    // erases the rectangle
+    pencil.clearRect(0,0, canvas.width, canvas.height);
+    
+    // draws cards
+    card1a.draw();
+    card1b.draw();
 }
 
 setInterval(gameLoop, 50);
-
-function detectClick() {
-    console.log("Clicked!")
-    card.click();
-
-}
-
-canvas.addEventListener("click", detectClick);
---
+++ memoryButton.js
@@ -0,0 +1,60 @@
+// import from tool box
+import { Toolbox } from "./toolbox.js";
+
+// exports memorybutton
+export class MemoryButton {
+
+    // card details
+    x = 50;
+    y = 50;
+    color;
+    width = 100;
+    height = 100;
+    canvas;
+    pencil;
+    isFaceUp = false; 
+    toolbox = new Toolbox();
+
+    constructor(canvas, pencil, x, y, color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.pencil = pencil;
+        this.canvas = canvas;
+        canvas.addEventListener("click", (e) => this.onClick(e));
+    }
+
+    // draws the cards 
+    draw() {
+        
+        if(this.isFaceUp) {
+            this.pencil.fillStyle = this.color; // Set the fill color
+            this.pencil.fillRect(this.x, this.y, this.width, this.height); // Draw a filled rectangle at (50, 50) with width 100 and height 75
+        } else { //draw face down
+            this.pencil.strokeStyle = "gray"; // Set the outline color to red
+            this.pencil.lineWidth = 10;       // Set the outline width to 2 pixels
+            this.pencil.strokeRect(this.x, this.y, this.width, this.height); // Draws an outlined rectangle at (50,50) with width 100 and height 75
+        }
+    }
+
+    // tracks clicking
+    onClick(event) {
+        let clickX = event.offsetX;
+        let clickY = event.offsetY;
+
+        let isClickInButton = this.toolbox.isWithinRect(
+            clickX, clickY, this.x, this.y, this.width, this.height
+        );
+
+        if(isClickInButton) {
+            this.isFaceUp = !this.isFaceUp;
+        }
+    }
+
+    
+    
+    
+
+
+
+}
